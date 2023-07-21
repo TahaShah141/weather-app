1d725349035971a4d8821c079366ed82bd4e4684
TahaShah141 <tahashah411@gmail.com>
2023-07-21 21:43:18 +0500

    Access the weatherAPI

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("hello")
+const baseURL = 'http://api.weatherapi.com/v1';
+const APIkey = '0c564310771b4d6b8d7145214232107';
+
+async function getWeatherData(location, option='current.json') {
+    let response = await fetch(`${baseURL}/${option}?key=${APIkey}&q=${location}`);
+    let Data = await response.json();
+    console.log(Data);
+}
